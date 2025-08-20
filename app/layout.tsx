a08a5b7d7a56bae59a22ab337f2c905fa4620a44
nikos <nikosdimouit@gmail.com>
2025-08-20 16:24:27 +0300

    dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+  <SidebarProvider>
+    <AppSidebar />
+    <html>
+      <body>
+        <SidebarTrigger />
         {children}
+
       </body>
     </html>
+  </SidebarProvider>
+    
   );
 }
